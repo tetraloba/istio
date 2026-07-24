f8e962e05ced99acdcf9f65f2d6a6fa88e9ec40c
tetraloba <four.leaves.siva7342@gmail.com>
2026-07-24 04:56:47 +0000

    fix update_istio_deps_ISTIO_PROXY_SHA.sh GIT_REMOTE_URL

--- update_istio_deps_ISTIO_PROXY_SHA.sh
+++ update_istio_deps_ISTIO_PROXY_SHA.sh
@@ -1,6 +1,6 @@
 # created by tetraloba (2026/07/24 12:45)
 # hugelobaのistioリポジトリにおいてistio.depsファイルを更新するためのスクリプト
-GIT_REMOTE_URL="https://github.com/tetraloba/istio.git"
+GIT_REMOTE_URL="https://github.com/tetraloba/proxy.git"
 BRANCH="feat/tetraloba"
 
 ISTIO_PROXY_SHA=$(git ls-remote ${GIT_REMOTE_URL} ${BRANCH} | cut -f 1)
